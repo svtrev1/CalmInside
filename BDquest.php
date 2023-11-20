7c390b640c86085e4accdb6e1a58283279cf0ede
KatenkaKet <katenkeket13@gmai.com>
2023-11-20 16:16:02 +0500

    add 'modal questionnairy'

---
+++ BDquest.php
@@ -0,0 +1,40 @@
+<?php
+
+$link = mysqli_connect('92.246.214.15:3306', 'ais_pashina1855_calminside', 'ltTIBfOaNBbbE956PPXXe1ce', 'ais_pashina1855_calminside');
+
+if (!$link)
+{
+    die("Connection fail". mysqli_connect_error());
+}
+
+$tension = $_GET['tension'];
+$attentiveness = $_GET['attentiveness'];
+$cheerfulness = $_GET['cheerfulness'];
+$comment = $_GET['comment'];
+$date = date('Y-m-d H:i:s');
+$id = -1;
+$radio = $_GET['radio'];
+
+$sql = mysqli_query($link, "SELECT * FROM `users`");
+while ($result = mysqli_fetch_array($sql)) {
+    if(strcmp($result['login'], $_COOKIE['login']) == 0){
+        $id = $result['id'];
+    }
+}
+
+
+if($id <> -1){
+    $sql = "INSERT INTO `questionnaire` (id_user, data,  mood, tension, attentiveness, cheerfulness, comment, bef_or_aft) VALUES ($id, '$date', '$radio', '$tension', '$attentiveness', '$cheerfulness', '$comment', '0')";
+    if ($link -> query($sql) == TRUE)
+    {
+        echo "Анкета заполнена успешно!";
+    }
+    else
+    {
+        echo "Ошибка: " . $link->error;
+    }
+}
+
+
+
+?>
--- user_profile.php
+++ user_profile.php
@@ -5,6 +5,7 @@
         header('Location: /CalmInside/main.php');
         die();
     }
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,7 +14,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="assets/modalquestionaire.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    
     <title>Личный кабинет</title>
 </head>
 <body>
@@ -21,5 +30,13 @@
         Поздравляем!!! Вы вошли в свой аккаунт, <?=$_COOKIE['login']?>
     </h1>
     <a href="/CalmInside/logout.php">Выйти</a>
+    <!-- Кнопка-триггер модального окна -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#questionnaire">Начать медитацию</button>
+
+    <?php
+        require('modalquestionnaire.php');
+    ?>
+
+    
 </body>
 </html>
